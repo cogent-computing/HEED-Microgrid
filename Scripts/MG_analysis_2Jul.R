815d3a84f391785b19e0cbd03aa5865d621a3fc6
Kriti Bhargava <ad0603@Kritis-MacBook-Pro-9623.local>
2020-07-03 19:38:04 +0100

    Created analysis script

--- Scripts/MG_analysis_2Jul.R
+++ Scripts/MG_analysis_2Jul.R
@@ -1,7 +1,7 @@
 #******************************************************************************************#
 # This is the script for analysing data for Community Hall                                 #
 # Author: K Bhargava                                                                       #
-# Last updated on: 2nd July 2020                                                           #
+# Last updated on: 3rd July 2020                                                           #
 #******************************************************************************************#
 
 #******************************************************************************************#
@@ -20,9 +20,10 @@ plot_dir <- "Plots/Paper 5"
 
 #******************************************************************************************#
 #Read in data files and plot data
-mg_imputed <- read.csv(here(filepath,"na_seadec_correctedData.csv"), 
+imputed_data <- read.csv(here(filepath,"na_seadec_correctedData.csv"), 
                          header=TRUE, stringsAsFactors = FALSE)
-mg_imputed <- mg_imputed %>% 
+imputed_data <- imputed_data %>% 
   mutate(date=as.Date(date),timestamp=as.POSIXct(timestamp,tz="GMT",origin="1970-01-01"),
          month = factor(month, levels = c("Jul", "Aug", "Sep", "Oct", "Nov", "Dec", "Jan", "Feb", "Mar"),
                         labels = c("Jul", "Aug", "Sep", "Oct", "Nov", "Dec", "Jan", "Feb", "Mar")))
+
